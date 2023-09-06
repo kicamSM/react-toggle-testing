8ea29843949b34af694b98d2ce5c6edfaebff4a7
kicamSM <kicamSMM@hotmail.com>
2023-09-06 13:10:04 -0600

    .

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,19 +1,19 @@
-// import React from 'react';
-// import '@testing-library/jest-dom/extend-expect'
-// // ! must have this piece of code or it will not work!!!!
-// import { render, fireEvent } from '@testing-library/react';
-// import Counter from './Counter'
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect'
+// ! must have this piece of code or it will not work!!!!
+import { render, fireEvent } from '@testing-library/react';
+import Counter from './Counter'
 
-// test('button clicks', () => {
-//     const {getByText, debug} = render(<span><Counter/></span>);
-//     debug();
-//     const h2 = getByText('Current count: 0');
-//     const btn = getByText('Add');
-//     fireEvent.click(btn);
-//     debugger;
-//     expect(h2).toHaveTextContent('1');
-//     expect(h2).not.toHaveTextContent('0');    
-// } )
+test('button clicks', () => {
+    const {getByText, debug} = render(<span><Counter/></span>);
+    debug();
+    const h2 = getByText('Current count: 0');
+    const btn = getByText('Add');
+    fireEvent.click(btn);
+    debugger;
+    expect(h2).toHaveTextContent('1');
+    expect(h2).not.toHaveTextContent('0');    
+} )
 
 // const {debug, getByText} = render(<Counter/>);
 // debug(); 
